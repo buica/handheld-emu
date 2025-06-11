92582cf3c093755311f900669818bb1b248693f9
calvin b <calvin.t.bui@gmail.com>
2025-06-11 04:55:18 -0700

    start loadRom

--- include/cart.hpp
+++ include/cart.hpp
@@ -2,6 +2,7 @@
 #define CART_HPP
 
 #include "common.hpp"
+#include <string>
 
 // To-Do
 // - [ ] Implement loadRom function
@@ -10,27 +11,27 @@
 class Cart
 {
 private:
-// cartridge headers (check out pan docs for full info)
-// header located at address range $0100 - $014F
-struct RomHeader
-{
-    u8 entry[4]; // $0100-$0103
-    u8 logo[0x30]; // $0104-$0133
-    char title[16]; // $0134-$0143
-    u8 newLicenseeCode; // $0144-$0145
-    u8 sgbFlag; // $0146
-    u8 cartType; // $0147
-    u8 romSize; // $0148
-    u8 ramSize; // $0149
-    u8 destCode; // $014A
-    u8 licenseeCode; // $014B, $33 means use new code
-    u8 romVersion; // $014C
-    u8 checksum; // $014D
-    u8 globalChecksum; // $014E-014F
-};
+    // cartridge headers (check out pan docs for full info)
+    // header located at address range $0100 - $014F
+    struct RomHeader
+    {
+        u8 entry[4]; // $0100-$0103
+        u8 logo[0x30]; // $0104-$0133
+        char title[16]; // $0134-$0143
+        u8 newLicenseeCode; // $0144-$0145
+        u8 sgbFlag; // $0146
+        u8 cartType; // $0147
+        u8 romSize; // $0148
+        u8 ramSize; // $0149
+        u8 destCode; // $014A
+        u8 licenseeCode; // $014B, $33 means use new code
+        u8 romVersion; // $014C
+        u8 checksum; // $014D
+        u8 globalChecksum; // $014E-014F
+    };
 
 public:
-void loadRom(const std::string& filepath);
-
+    void loadRom(const std::string& filepath);
+};
 
 #endif
---
+++ src/cart.cpp
@@ -0,0 +1,17 @@
+#include "../include/cart.hpp"
+#include <vector>
+#include <fstream>
+
+
+
+void loadRom(const std::string& filepath) {
+    // assert file is .gb
+    // load rom data into array/vector
+    //
+    std::vector<u8> romData;
+    std::ifstream file(filepath, std::ios::binary); //open in binary mode to get raw bytes
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open file");
+    }
+
+}
