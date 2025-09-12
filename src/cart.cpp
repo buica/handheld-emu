0f9cc238f9321cfd190115f9ee71c153959104bb
Calvin B <calvin.t.bui@gmail.com>
2025-09-12 00:02:55 -0700

    implement Cart's readByte and start Cart unit tests
    Implemented readByte and getRomData methods for Cart class. Completed simple unit test for Cart::load() which passed. Unit tests for readByte and getRomData incomplete.

--- src/cart.cpp
+++ src/cart.cpp
@@ -1,4 +1,5 @@
 #include "../include/cart.hpp"
+#include "../include/memory.hpp"
 #include <iostream>
 #include <fstream>
 #include <filesystem>
@@ -6,7 +7,6 @@
 
 /**
  * @brief Loads a ROM file into memory.
- *
  * @param filepath The path to the ROM file.
  * @return True if load succeeds.
  */
@@ -35,11 +35,35 @@ bool Cart::load(const std::string& filepath) {
     return true;
 }
 
-/*
-* @brief
-* @param
-* @return description of the return value description
-*/
-u8 readByte(u16 address) {
 
+/**
+* @brief returns ROM data loaded in memory
+* @return the ROM data vector
+*/
+std::vector<u8>& Cart::getRomData() {
+    return m_rom_data;
+}
+
+
+/**
+* @brief Read a byte from the cartridge address space, and route the read to
+* ROM bank 00, switchable ROM banks, or external RAM depending on the address.
+* @param address The address from cartridge space to read from
+* @return The byte read from the cartridge.
+*/
+u8 Cart::readByte(u16 address) {
+    if (address >= ROM_BANK_00_START && address <= ROM_BANK_00_END) {
+        // 16 KiB ROM bank 00 (fixed)
+        return m_rom_data[address];
+    } else if (address >= ROM_BANK_NN_START && address <= ROM_BANK_NN_END) {
+        // 16 KiB switchable ROM bank
+        // for now, just read from next 16KiB block
+        return m_rom_data[address];
+    } else if (address >= EXT_RAM_START && address <= EXT_RAM_END) {
+        // 8 KiB External RAM
+        // to-do: implement external RAM banking
+        return 0xFF;
+    } else {
+        std::cerr << "Cartridge read from invalid address: 0x" << std::hex << address << std::endl;
+    }
 }
--- tests/cart_tests.cpp
+++ tests/cart_tests.cpp
@@ -7,7 +7,7 @@
 TEST_CASE("Test: Cart Functions") {
     // create and populate dummy ROM
     const char* test_rom_path = "test_cart.gb";
-    std::vector<u8> test_rom_data = {0x00, 0x01, 0x10} // NOP; LD BC, n16; STOP
+    std::vector<u8> test_rom_data = {0x00, 0x01, 0x10}; // NOP; LD BC, n16; STOP
 
     std::ofstream output_file(test_rom_path, std::ios::binary);
     output_file.write(reinterpret_cast<const char*>(test_rom_data.data()), test_rom_data.size());
@@ -16,7 +16,9 @@ TEST_CASE("Test: Cart Functions") {
     Cart cart;
 
     SECTION("Test: Cart Load ROM") {
-        REQUIRE(cart.load("path/to/rom/file") == true);
+        REQUIRE(cart.load("test_cart.gb") == true);
     }
+
+    std::remove("test_cart.gb");
 
 }
