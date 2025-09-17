b0f14cff58a51c58ab41ca16b85807f934cf3466
Calvin B <calvin.t.bui@gmail.com>
2025-09-17 02:36:19 -0700

    complete unit tests for Cart's getRomData and readByte methods

--- tests/cart_tests.cpp
+++ tests/cart_tests.cpp
@@ -7,6 +7,7 @@
 TEST_CASE("Test: Cart Functions") {
     // create and populate dummy ROM
     const char* test_rom_path = "test_cart.gb";
+    const char* non_existing_rom_path = "non_existing_cart.gb";
     std::vector<u8> test_rom_data = {0x00, 0x01, 0x10}; // NOP; LD BC, n16; STOP
 
     std::ofstream output_file(test_rom_path, std::ios::binary);
@@ -15,8 +16,25 @@ TEST_CASE("Test: Cart Functions") {
 
     Cart cart;
 
-    SECTION("Test: Cart Load ROM") {
-        REQUIRE(cart.load("test_cart.gb") == true);
+    SECTION("Test: Cart Load ROM Suceeds") {
+        REQUIRE(cart.load(test_rom_path) == true);
+    }
+    SECTION("Test: Cart Load ROM Fails") {
+        REQUIRE(cart.load(non_existing_rom_path) == false);
+    }
+
+    SECTION("Test: Get ROM Data from Cart") {
+        REQUIRE(cart.load(test_rom_path) == true);
+        auto& rom = cart.getRomData();
+        REQUIRE(rom == test_rom_data);
+    }
+
+    SECTION("Test: Read Byte from ROM Bank 00") {
+        REQUIRE(cart.load(test_rom_path) == true);
+        REQUIRE(cart.readByte(0x0000) == 0x00); // NOP
+        REQUIRE(cart.readByte(0x0001) == 0x01); // LD BC, n16
+        REQUIRE(cart.readByte(0x0002) == 0x10); // STOP
+        REQUIRE(cart.readByte(0x0003) == 0xFF); // out of bounds, should return 0xFF
     }
 
     std::remove("test_cart.gb");
